028f6fd225fca2ab1a17fc88c14cee8ab2145d22
WallaceColeman <wallardcoleman@gmail.com>
2020-01-28 14:18:53 -0500

    Added point lights to illuminate the podium

--- js/main.js
+++ js/main.js
@@ -11,6 +11,31 @@ camera.position.z= 10;
 var light = new THREE.AmbientLight( 0x404040, 10.0 ); // soft white light
 scene.add( light );
 
+
+var pLightR = new THREE.PointLight( 0xff0000, 10.0, 100 );
+pLightR.position.x = 50;
+pLightR.position.y = 50;
+pLightR.position.z = 10;
+scene.add( pLightR );
+
+var pLightY = new THREE.PointLight( 0xffff00, 10.0, 100 );
+pLightY.position.x = -50;
+pLightY.position.y = 50;
+pLightY.position.z = 10;
+scene.add( pLightY );
+
+var pLightB = new THREE.PointLight( 0x0000ff, 10.0, 100 );
+pLightB.position.x = -50;
+pLightB.position.y = -50;
+pLightB.position.z = 10;
+scene.add( pLightB );
+
+var pLightW = new THREE.PointLight( 0xffffff, 10.0, 100 );
+pLightW.position.x = 50;
+pLightW.position.y = -50;
+pLightW.position.z = 10;
+scene.add( pLightW );
+
 //GLTF loader code provided by Three.js
 //https://threejs.org/docs/#examples/en/loaders/GLTFLoader
 //Accessed: 01/25/2020
@@ -49,38 +74,27 @@ loader.load(
 );
 
 loader.load(
-	// resource URL
 	'js/Models/podium.gltf',
-	// called when the resource is loaded
 	function ( gltf ) {
 
 		scene.add( gltf.scene );
 		
 		model2 = gltf.scene;//!!!! saves the model into a varible for manipulation
-		model2.position.y = (-2);
-		
-		
-		// gltf.animations; // Array<THREE.AnimationClip>
-		// gltf.scene; // THREE.Scene
-		// gltf.scenes; // Array<THREE.Scene>
-		// gltf.cameras; // Array<THREE.Camera>
-		// gltf.asset; // Object
+		model2.position.y = (-2.2);
+		model2.rotation.y = 45+(45/2);
 
 	},
-	// called while loading is progressing
 	function ( xhr ) {
 
 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
 	},
-	// called when loading has errors
 	function ( error ) {
 
 		console.log( 'An error happened' );
 
 	}
 );
-
 
 function render(){
   requestAnimationFrame (render);
