f1c6b327ee02a9944d6edcb36d7686fee0ca4d29
Allen Kubinski <adk0928@yahoo.com>
2020-02-14 13:46:22 -0500

    Completely reworked Allens_Test to use planes as a room

--- js/Allens_Test.js
+++ js/Allens_Test.js
@@ -1,197 +1,252 @@
-'use strict';
+"use strict";
 
-//add some physics
 Physijs.scripts.worker = '/js/ThreeLib/physijs_worker.js';
 Physijs.scripts.ammo = "http://chandlerprall.github.io/Physijs/examples/js/ammo.js";
 
-var LOADING_NOT_DONE = true;
-var loadingManager = null;
-var amount_loaded = 0.0;
+var scene = new Physijs.Scene;
+scene.setGravity(new THREE.Vector3(0,-25,0));
 
-var renderer;
-var scene;
-var camera;
-var player;
 
-//--------------------------------Loading Screen--------------------------------
-	//Based on: xSaucecode
-	//Loaction: https://www.youtube.com/watch?v=3umV-dEYttU&ab_channel=xSaucecode
-	//Posted:   08/05/2016
-	//Accessed: 02/08/2020
-var loading = {
-	scene: new THREE.Scene(),
-	camera: new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1,1000),
-	box: new THREE.Mesh(
-		new THREE.BoxGeometry(1,1,1),
-		new THREE.MeshBasicMaterial({color:"rgb(255,0,0)"})
-	)
-};
-//--------------------------------Loading Screen--------------------------------
+var camera = new THREE.PerspectiveCamera(70,window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.x = 0;
+camera.position.y = 30;
+camera.position.z = 100;
+camera.lookAt(scene.position);
 
-function setUp(){
-	
-	//--------------------------------Loading Screen--------------------------------
-	//Based on: xSaucecode
-	//Loaction: https://www.youtube.com/watch?v=3umV-dEYttU&ab_channel=xSaucecode
-	//Posted:   08/05/2016
-	//Accessed: 02/08/2020
-	loading.box.position.set(0,0,0);
-	loading.camera.lookAt(loading.box.position);
-	loading.camera.position.z=10;
-	loading.scene.add(loading.box);
-	loading.scene.add(loading.camera);
-	loading.scene.add( new THREE.AmbientLight( 0x404040) );
+//SpotLight
+var spotLight = new THREE.SpotLight(0xffffff);
+spotLight.position.set(-40,60,40);
+spotLight.castShadow = true;
+scene.add(spotLight); 
 
-	loadingManager = new THREE.LoadingManager();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor("rgb(135,206,235)");//skyblue
+renderer.setSize(window.innerWidth-20, window.innerHeight-20);
 
-	loadingManager.onProgress = function(item, loaded, total){
-		console.log(item, total, loaded);
-		amount_loaded = (loaded/31); //31 should be changed to the total number of things that needs to be loaded. this can be found in console after running the first time.
-	}
-	loadingManager.onLoad = function(){
-		console.log("Loaded");
-		LOADING_NOT_DONE = false;
-	}
-	//--------------------------------Loading Screen--------------------------------
 
-    scene = new Physijs.Scene();
-    scene.setGravity(new THREE.Vector3(0,-25,0));
+//floor
 
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1,1000);
-	
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize (window.innerWidth-20, window.innerHeight-20);
-	renderer.outputEncoding = THREE.sRGBEncoding;
-	document.body.appendChild(renderer.domElement);
-	
-	camera.position.z= 100;
-	var light = new THREE.AmbientLight( 0x404040, 10.0 ); // soft white light
-	scene.add( light );
-
-    var loader = new THREE.GLTFLoader(loadingManager);
-    
-    loader.load(//RoomTest
-		// resource URL
-		'../../Models/Levels/RoomTemplate.glb',
-		// called when the resource is loaded
-		function ( gltf ) {
-
-			scene.add( gltf.scene );
-			
-			let room = gltf.scene; //!!!!! saves for manipulation
-			room.position.y = 1;
-			room.position.x = 1;
-			room.scale.x = 100;
-			room.scale.y = 100;
-            room.scale.z = 100;
-            
-            room.addEventListener('collision', function( objCollidedWith, linearVelOfCollision, angularVelOfCollision ) {
-            });
-
-		},
-		// called while loading is progressing
-		function ( xhr ) {
-
-			//console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-		},
-		// called when loading has errors
-		function ( error ) {
-
-			console.log( 'A room test error happened' );
-
-		}
-	);
-
-//Plane
-var planeGeometry = new THREE.PlaneGeometry(100,100,1,1);
-var planeMaterial = new THREE.MeshBasicMaterial({color:0x00FF00});
-
+var planeGeometry = new THREE.PlaneGeometry(250,150,1,1);
+//var planeMaterial = new THREE.MeshBasicMaterial({color:"rgb(10,200,10)"});
+let planeMaterial = Physijs.createMaterial(
+    new THREE.MeshBasicMaterial({ color:"rgb(10,10,200)"}),
+    0.2,
+    0.2
+);
 var plane = new Physijs.BoxMesh(planeGeometry, planeMaterial);
-plane.rotation.x = 0.5*Math.PI;
+plane.rotation.x = -.5*Math.PI;
+//plane.rotation.y = (0.125)*Math.PI;
+scene.add(plane);
+
+//front wall
+plane = new Physijs.BoxMesh(planeGeometry, planeMaterial);
+plane.rotation.x = -1*Math.PI;
+plane.position.z = 75;
+scene.add(plane);
+
+//back wall
+plane = new Physijs.BoxMesh(planeGeometry, planeMaterial);
+plane.position.z = -50;
+scene.add(plane);
+
+//left wall
+plane = new Physijs.BoxMesh(planeGeometry, planeMaterial);
+plane.rotation.y = .5*Math.PI;
+plane.position.x = -75;
+scene.add(plane);
+
+//right wall
+
+plane = new Physijs.BoxMesh(planeGeometry, planeMaterial);
+plane.rotation.y = -.5*Math.PI;
+plane.position.x = 75;
 scene.add(plane);
 
 //Cube
-var cubeGeometry = new THREE.CubeGeometry(6,6,6);
-//var cubeMaterial = new THREE.MeshLambertMaterial({color:red});
-var cubeMaterial = Physijs.createMaterial(
-    new THREE.MeshBasicMaterial({ color: 0x241BB6}),
-    0.5,
-    0.5
+//Cube - Code created by chandlerprall
+var block_material = Physijs.createMaterial(
+    new THREE.MeshBasicMaterial({ color:0xFF0000}),
+    .4, // medium friction
+    .4 // medium restitution
 );
-var cube = new Physijs.BoxMesh(cubeGeometry, cubeMaterial);
-cube.castShadow = true;
+
+block_material.castShadow = true;
+
+var blockGeometry = new THREE.BoxGeometry(6, 6, 6);
+var cube = new Physijs.BoxMesh(blockGeometry, block_material)
 cube.receiveShadow = true;
-cube.position.x = -10;
-cube.position.y = 30;
+cube.castShadow = true;
+cube.position.y = 3;
+cube.position.x = -5
 scene.add(cube);
-cube.addEventListener('collision', function( objCollidedWith, linearVelOfCollision, angularVelOfCollision ) {
-});
+
+var moveIn = false;
+var moveOut = false;
+var moveLeft = false;
+var moveRight = false;
+
+var reset = false;
 
 
-
-var maxVelocity = 15;
-player = cube.id;
-var ground = plane.position.y;
+var player = cube.id;
 document.addEventListener("keydown", onDocumentKeyDown, false);
 function onDocumentKeyDown(event) {
-   var keyCode = event.which;
-   //var velocity = new THREE.Vector3();
-   var velocity = scene.getObjectById(player).getLinearVelocity();
-   if (keyCode == 87) {//w
-       //scene.getObjectById(player).se
-       velocity.z -= 0.5; 
-   } else if (keyCode == 83) {//s
-       //scene.getObjectById(player).position.y -= ySpeed;
-       velocity.z += 0.5; 
-   } else if (keyCode == 65) {//a
-       //scene.getObjectById(player).position.x -= xSpeed;
-       velocity.x -= 0.5; 
-   } else if (keyCode == 68) {//d
-       //scene.getObjectById(player).position.x += xSpeed;
-       velocity.x += 0.5; 
-   } else if (keyCode == 82) {//Letter "r: to restart, might change later
-       scene.getObjectById(player).position.set(0, 30, 0);
-       scene.getObjectById(player).__dirtyPosition = true;
-       velocity = new THREE.Vector3()
-       velocity.x = 15;
-       velocity.y = 20;
-   } else if (keyCode == 32 && velocity.y < maxVelocity) {//Space Bar, to jump
-        velocity.y += 15;
-   }
-   scene.getObjectById(player).setLinearVelocity(velocity);
+    var keyCode = event.which;
+    if (keyCode == 87) {//w
+        moveIn = true;
+    } else if (keyCode == 83) {//s
+        moveOut = true;
+    } else if (keyCode == 65) {//a
+        moveLeft = true;
+    } else if (keyCode == 68) {//d
+        moveRight = true;
+    }
+    
 };
 
-
-
+document.addEventListener("keyup", onDocumentKeyUp, false);
+function onDocumentKeyUp(){
+    var keyCode = event.which;
+    if (keyCode == 87) {//w
+        moveIn = false;
+    } else if (keyCode == 83) {//s
+        moveOut = false;
+    } else if (keyCode == 65) {//a
+        moveLeft = false;
+    } else if (keyCode == 68) {//d
+        moveRight = false;
+    }
+    else if(keyCode == 82) {//r
+        reset = true;
+        // while(lock == 2){/*wait*/}
+        // lock = 1;
+        //console.log("AS");
+        var v = new THREE.Vector3(0,0,0);
+        scene.getObjectById(player).setLinearFactor(v);
+        scene.getObjectById(player).setAngularFactor(v);
+        // scene.getObjectById(player).setLinearVelocity(v);
+        // scene.getObjectById(player).setAngularVelocity(v);
+        
+        scene.getObjectById(player).__dirtyPosition = true;
+        scene.getObjectById(player).position.set(0, 30, 0);
+        v.x = 15;
+        v.y = 20;
+        v.z = 0;
+        scene.getObjectById(player).setLinearFactor(new THREE.Vector3(1,1,1));
+        scene.getObjectById(player).setAngularFactor(new THREE.Vector3(1,1,1));
+        scene.getObjectById(player).setLinearVelocity(v);
+        //console.log("AE");
+        // if(lock == 1){
+        //     lock=0;
+        // }
+        // else{
+        //     console.log("Broken spinlock");
+        // }
+    }
 }
 
- 
+function slide_controlls(){//SWITCH TO applyCentralImpulse*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!**88
+    // while(lock == 1){/*wait*/}
+    // lock = 2;
+    // if(reset){
+    //     var v = new THREE.Vector3();
+    //     v.x = 0;
+    //     v.y = 0;
+    //     v.z = 0;
+    //     scene.getObjectById(player).setLinearVelocity(v);
+    //     scene.getObjectById(player).setAngularVelocity(v);
+        
+    //     scene.getObjectById(player).__dirtyPosition = true;
+    //     scene.getObjectById(player).__dirtyRotation = true;
+    //     scene.getObjectById(player).position.set(0, 30, 0);
+    //     scene.getObjectById(player).rotation.set(0, 0, 0);
+    //     v.x = 15;
+    //     v.y = 20;
+    //     v.z = 0;
+    //     scene.getObjectById(player).setLinearVelocity(v);
+    //     reset = false;
+    //     return;
+    // }
+    //console.log("BS");
+    if(reset){
+        // let t = new Date().getTime();
+        // while(new Date().getTime() - t < 1000){}
+        // reset = false;
+        // return;
+    }
+    let velocity = new THREE.Vector3();
+    let cv = scene.getObjectById(player).getLinearVelocity();
+    if(moveIn){
+        if(cv.z > 0){
+            velocity.z -= 1000;
+        }
+        else if(cv.z > -50){
+            velocity.z -= 100;
+        }
+        //scene.getObjectById(player).applyCentralImpulse(velocity);
+        //console.log("in");
+    }
+    if(moveOut){
+        if(cv.z < 0){
+            velocity.z += 1000;
+        }
+        else if(cv.z < 50){
+            velocity.z += 100;
+        }
+        //scene.getObjectById(player).setLinearVelocity(velocity);
+        //console.log("out");
+    }
+    if(moveLeft){
+        if(cv.x > 0){
+            velocity.x -= 1000;
+        }
+        else if(cv.x > -50){
+            velocity.x -= 100;
+        }
+        //scene.getObjectById(player).setLinearVelocity(velocity);
+        //console.log("right");
+    }
+    if(moveRight){
+        if(cv.x < 0){
+            velocity.x += 1000;
+        }
+        else if(cv.x < 50){
+            velocity.x += 100;
+        }
+        //scene.getObjectById(player).setLinearVelocity(velocity);
+        //console.log("left");
+    }
+    //********************************************************************************************************
+    //********************************************************************************************************
+    //********************************************************************************************************
+    // every frame is calling the setlinearvelocity function, it's probaly screwing with the physics
+    //********************************************************************************************************
+    //********************************************************************************************************
+    //********************************************************************************************************
+    //scene.getObjectById(player).setLinearVelocity(velocity);
+    scene.getObjectById(player).applyCentralImpulse(velocity);
+    //console.log("BE");
+    // if(lock == 2){
+    //     lock=0;
+    // }
+    // else{
+    //     console.log("Broken spinlock");
+    // }
+        
+    //console.log("none");
+        
+}
 
-function render(){
+function renderScene(){
 
     scene.simulate();
-  requestAnimationFrame (render);
-  camera.position.x = scene.getObjectById(player).position.x;
-        camera.position.y = scene.getObjectById(player).position.y+25;
-  renderer.render (scene, camera);
+    requestAnimationFrame(renderScene);
+    slide_controlls();
+    camera.position.x = scene.getObjectById(player).position.x;
+    camera.position.y = scene.getObjectById(player).position.y+25;
+    renderer.render(scene, camera);
 }
 
-function loadingRenderer(){
-	//--------------------------------Loading Screen--------------------------------
-	//Based on: xSaucecode
-	//Loaction: https://www.youtube.com/watch?v=3umV-dEYttU&ab_channel=xSaucecode
-	//Posted:   08/05/2016
-	//Accessed: 02/08/2020
-	if(LOADING_NOT_DONE){
-		requestAnimationFrame (loadingRenderer);
-		loading.box.scale.x = amount_loaded*20;//*20 to make the bar take up more of the screen.
-		renderer.render (loading.scene, loading.camera);
-	}//--------------------------------Loading Screen--------------------------------
-	else{
-		render();
-	}
-}
+document.body.appendChild(renderer.domElement);
 
-setUp();
-loadingRenderer();
+renderScene();
